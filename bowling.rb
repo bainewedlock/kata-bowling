866aeb7c5a2e6e5df64c45e41f97137d0f5a16d4
boeckwi <w.boeck@walz.de>
2018-05-14 13:35:13 +0200

    refactored recursion to loop

--- bowling.rb
+++ bowling.rb
@@ -6,26 +6,24 @@ class Bowling
     @rolls.push pins
   end
   def score
-    calc_frames @rolls, 10
-  end
-
-  private
-
-  def calc_frames(rolls, remaining_frames)
-    return 0 if remaining_frames == 0
-
-    if strike_at? rolls
-      skip = 1
-      bonus = 2
-    elsif spare_at? rolls
-      skip = 2
-      bonus = 1
-    else
-      skip = 2
-      bonus = 0
+    total = 0
+    frame_index = 0
+    10.times do
+      remaining_rolls = @rolls.drop(frame_index)
+      if strike_at? remaining_rolls
+        skip = 1
+        bonus = 2
+      elsif spare_at? remaining_rolls
+        skip = 2
+        bonus = 1
+      else
+        skip = 2
+        bonus = 0
+      end
+      total += sum(remaining_rolls, skip + bonus)
+      frame_index += skip
     end
-    sum(rolls, skip + bonus) +
-      calc_frames(rolls.drop(skip), remaining_frames-1)
+    total
   end
   def strike_at?(rolls)
     sum(rolls, 1) == 10
